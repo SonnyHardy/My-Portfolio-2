e993ae447ca8adc16456b0c6f2e7f6bede2dfc45
SonnyHardy <htambeck@gmail.com>
2026-02-23 18:06:52 +0100

    Add core sections, Navbar, and global styles to structure the app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import Navbar from "./layout/Navbar.tsx";
+import Hero from "./sections/Hero.tsx";
+import About from "./sections/About.tsx";
+import Projects from "./sections/Projects.tsx";
+import Testimonials from "./sections/Testimonials.tsx";
+import Contact from "./sections/Contact.tsx";
+import Experience from "./sections/Experience.tsx";
 
 function App() {
 
   return (
-    <></>
+    <div className="min-h-screen overflow-x-hidden">
+        <Navbar />
+        <main>
+            <Hero />
+            <About />
+            <Projects />
+            <Experience />
+            <Testimonials />
+            <Contact />
+        </main>
+    </div>
   )
 }
 
---
+++ src/sections/Projects.tsx
@@ -0,0 +1,4 @@
+
+export default function Projects() {
+  return <section>Projects</section>
+}
