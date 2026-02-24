b4b9ad6f50b1c13f3a2c292aa4d84f7437decb7f
SonnyHardy <htambeck@gmail.com>
2026-02-24 13:53:06 +0100

    Implementation of the Hero section

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -26,7 +26,7 @@ export default function Navbar() {
     }, []);
 
     return (
-        <header className={`fixed top-0 left-0 right-0 transition-all duration-300
+        <header className={`fixed top-0 left-0 right-0 transition-all duration-300 border-none
               ${isScrolled ? "glass-strong py-3" : "bg-transparent py-5"} z-50`}>
             <nav className="container mx-auto px-6 flex items-center justify-between md:justify-around">
                 <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
